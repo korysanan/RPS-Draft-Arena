1a329b1be212c49c42eecde235d8ab3880906156
Lee WooJin <ujin0561@naver.com>
2026-04-27 23:39:40 +0900

    Single Play Mode UI 작업
    모드 버튼 배치 및 UI 작업

---
+++ Assets/Scripts/Single_Play_Home_Manager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Single_Play_Home_Manager : MonoBehaviour
+{
+    public void OnPracticeModeClicked()
+    {
+        Debug.Log("Practice Mode Button Clicked");
+    }
+
+    public void OnTournamentModeClicked()
+    {
+        Debug.Log("Tournament Mode Button Clicked");
+    }
+
+    public void OnLeagueModeClicked()
+    {
+        Debug.Log("League Mode Button Clicked");
+    }
+
+    public void OnChallengeModeClicked()
+    {
+        Debug.Log("Challenge Mode Button Clicked");
+    }
+}
